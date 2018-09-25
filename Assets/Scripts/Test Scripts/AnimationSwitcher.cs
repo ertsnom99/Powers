ba22c12c6016acefb3339e5a3f06b0704e7acda3
Kévin Duguay <kevin.duguay1@gmail.com>
2018-09-25 04:18:37 -0400

    Separated left and right arm animations

---
+++ Assets/Scripts/Test Scripts/AnimationSwitcher.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class AnimationSwitcher : MonoBehaviour
+{
+    private Animator m_animator;
+    
+    protected int m_isHoldingLeftSphereParamHashId = Animator.StringToHash(IsHoldingLeftSphereParamNameString);
+    protected int m_isHoldingRightSphereParamHashId = Animator.StringToHash(IsHoldingRightSphereParamNameString);
+    
+    public const string IsHoldingLeftSphereParamNameString = "IsHoldingLeftSphere";
+    public const string IsHoldingRightSphereParamNameString = "IsHoldingRightSphere";
+
+    private void Awake()
+    {
+        m_animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            m_animator.SetBool(m_isHoldingLeftSphereParamHashId, true);
+        }
+        else if(Input.GetKeyDown("a"))
+        {
+            m_animator.SetBool(m_isHoldingLeftSphereParamHashId, false);
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            m_animator.SetBool(m_isHoldingRightSphereParamHashId, true);
+        }
+        else if (Input.GetKeyDown("s"))
+        {
+            m_animator.SetBool(m_isHoldingRightSphereParamHashId, false);
+        }
+    }
+}
