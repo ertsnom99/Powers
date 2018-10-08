cb779a74ae8962c6a0d51f071e86796688d173fa
Kévin Duguay <kevin.duguay1@gmail.com>
2018-10-08 01:41:01 -0400

    Made teleport not chargeable

--- Assets/Scripts/Powers/Teleport.cs
+++ Assets/Scripts/Powers/Teleport.cs
@@ -6,6 +6,13 @@ public class Teleport : Power
     [SerializeField]
     private GameObject m_teleportProjectile;
 
+    protected override void Awake()
+    {
+        base.Awake();
+
+        IsChargeable = false;
+    }
+
     public override void ShowPower(bool show)
     {
         m_teleportProjectile.SetActive(show);
